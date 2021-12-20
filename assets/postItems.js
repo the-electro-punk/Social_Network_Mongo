32fe3342dc1e66514f7cf9937c0abb80fc315c3f
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2021-12-19 20:41:35 -0600

    updated files

--- assets/postItems.js
+++ assets/postItems.js
@@ -1,4 +1,17 @@
-postTitle = document.getElementById("titleInput")
-postContent = document.getElementById("postInput")
+let postTitle = document.getElementById("titleInput")
+let postContent = document.getElementById("postInput")
+let list_posts = document.getElementById("list_posts")
 
-submit = document.getElementById("postBTN")
+let loggedPosts = []
+
+let submit = document.getElementById("postBTN")
+
+submit.addEventListener('click', submitPost)
+
+function submitPost() {
+    for (var i = 0; i < loggedPosts; i++) {
+        let newPost = document.createElement('li')
+        newPost.textContent = (postTitle.value + '<b>' + postContent.value)
+        list_posts.textContent = newPost
+    }
+}
